428b897f8c6c23196140e5b01866f9ee810bfe9a
Meyzhrl <alzahrameyda@gmail.com>
2023-10-12 16:53:42 +0700

    add index.html

---
+++ main.js
@@ -0,0 +1,19 @@
+// console.log("Hello World")
+// alert("ada Notifikasi")
+// prompt("pinjam seratus")
+
+// var promnet = "coding is easy"
+// console.log(promnet)--bisa 2 kali
+
+// let promnet = "coding is fun"
+// console.log(promnet)--tidak bisa 2kali
+
+// const promnet ="coding is my life"
+// console.log(promnet)
+
+let totalpoin = prompt("masukkan poin anda")
+    if (totalpoin > 100){
+        document.write("congratulation");
+        }else{
+        document.write("keep trying");
+    }
